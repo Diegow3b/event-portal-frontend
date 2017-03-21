6e5cc5c446e3e3b9e8efd7cf9b743eb35bc35c2b
Diego Vinicius <diegowebd@hotmail.com>
2017-03-20 23:27:20 -0300

    setting up prod mod

--- src/app/services/eventos.service.ts
+++ src/app/services/eventos.service.ts
@@ -5,40 +5,43 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class EventosService {
 
-  constructor(private http: Http) {
-    console.log('Evento Service Initialized...');
-  }
+    private host: string;
 
-  getEventos() {
-      return this.http.get('http://localhost:3000/api/eventos')
-          .map(res => res.json());
-  }
+    constructor(private http: Http) {
+        console.log('Evento Service Initialized...');
+        this.host = process.env.PROD || 'http://localhost:3000';
+    }
 
-  filterEvento(filterObj){
-      var headers = new Headers();
-      headers.append('Content-Type', 'application/json');
-      console.log(filterObj);
-      return this.http.post('http://localhost:3000/api/eventos/filter', JSON.stringify(filterObj), { headers: headers })
-          .map(res => res.json());
-  }
+    getEventos() {
+        return this.http.get(this.host+'/api/eventos')
+            .map(res => res.json());
+    }
 
-  addEvento(newEvento) {
-      var headers = new Headers();
-      headers.append('Content-Type', 'application/json');
-      return this.http.post('http://localhost:3000/api/eventos', JSON.stringify(newEvento), { headers: headers })
-          .map(res => res.json());
-  }
+    filterEvento(filterObj) {
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        console.log(filterObj);
+        return this.http.post(this.host+'/api/eventos/filter', JSON.stringify(filterObj), { headers: headers })
+            .map(res => res.json());
+    }
 
-  deleteEvento(id){
-      return this.http.delete('http://localhost:3000/api/eventos/' + id)
-          .map(res => res.json());
-  }
+    addEvento(newEvento) {
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return this.http.post(this.host+'/api/eventos', JSON.stringify(newEvento), { headers: headers })
+            .map(res => res.json());
+    }
 
-  updateEvento(evento){
-      var headers = new Headers();
-      headers.append('Content-Type', 'application/json');
-      return this.http.put('http://localhost:3000/api/eventos/'+evento._id, JSON.stringify(evento), { headers: headers })
-          .map(res => res.json());
-  }
+    deleteEvento(id) {
+        return this.http.delete(this.host+'/api/eventos/' + id)
+            .map(res => res.json());
+    }
+
+    updateEvento(evento) {
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return this.http.put(this.host+'/api/eventos/' + evento._id, JSON.stringify(evento), { headers: headers })
+            .map(res => res.json());
+    }
 
 }
