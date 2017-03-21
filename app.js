1b6d25c7aeba6115ddffdf4ad8404a24f275cf66
Diego Vinicius <diegowebd@hotmail.com>
2017-03-20 23:48:45 -0300

    trying with dist folder in app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist'));
+
+const path = require('path');
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
