5d4a48d70e32cdc733b584c93835f3fceefd0074
diego <diegowebd@hotmail.com>
2017-03-31 09:16:05 -0300

    Creating User Class

---
+++ class/user.ts
@@ -0,0 +1,13 @@
+export class User {
+    _id: string;
+    email: string;
+    username: string;
+    password: string;
+    created_at: Date;
+    is_administrator: boolean;
+    is_active: boolean;
+
+    constructor() {
+        this.is_active = true;
+    }
+}
